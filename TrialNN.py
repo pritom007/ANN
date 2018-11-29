50803f5818412c1d2a8548ed5653a978c2518cbe
Pritom007 <naitiq@yahoo.com>
2018-11-29 20:43:40 +0800

    infrance function

--- TrialNN.py
+++ TrialNN.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 # X = (hours sleeping, hours studying), y = score on test
-X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-y = np.array(([92], [86], [89]), dtype=float)
-print(X)
+X = np.array(([1, 0], [0, 1], [0, 0]), dtype=float)
+y = np.array(([1], [1], [0]), dtype=float)
+newData = np.asarray(([1,1]),dtype=float)
 # scale units
-X = X/np.amax(X, axis=0) # maximum of X array
+#X = X/np.amax(X, axis=0) # maximum of X array
 
-y = y/100 # max test score is 100
+#y = y/100 # max test score is 100
 
 class Neural_Network(object):
   def __init__(self):
@@ -15,16 +15,22 @@ class Neural_Network(object):
     self.inputSize = 2
     self.outputSize = 1
     self.hiddenSize = 4
-
+    sizes = [2, 4, 1]
+    self.biases0 = np.random.rand(3, 4)
+    print(self.biases0)
+    self.biases1 = np.random.rand(3, 1)
     #weights
     self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (3x2) weight matrix from input to hidden layer
     self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (2x1) weight matrix from hidden to output layer
 
+
+
   def forward(self, X):
     #forward propagation through our network
-    self.z = np.dot(X, self.W1) # dot product of X (input) and first set of 3x2 weights
+    self.z = np.dot(X, self.W1) + self.biases0# dot product of X (input) and first set of 3x2 weights
     self.z2 = self.sigmoid(self.z) # activation function
-    self.z3 = np.dot(self.z2, self.W2) # dot product of hidden layer (z2) and second set of 3x1 weights
+
+    self.z3 = np.dot(self.z2, self.W2)+ self.biases1 # dot product of hidden layer (z2) and second set of 3x1 weights
     o = self.sigmoid(self.z3) # final activation function
     return o
 
@@ -51,7 +57,17 @@ class Neural_Network(object):
     o = self.forward(X)
     self.backward(X, y, o)
 
+def argmax(array):
+  hi = 0
+  for x in array:
+    if x > hi:
+      hi = x
+  return str(hi)
+
+
 NN = Neural_Network()
+#print(np.shape(NN.biases[1]))
+#print(NN.biases[0]," oo" ,NN.biases[1])
 for i in range(1000): # trains the NN 1,000 times
   print ("Input: \n" + str(X) )
   print ("Actual Output: \n" + str(y))
@@ -59,3 +75,6 @@ for i in range(1000): # trains the NN 1,000 times
   print ("Loss: \n" + str(np.mean(np.square(y - NN.forward(X))))) # mean sum squared loss
   print ("\n")
   NN.train(X, y)
+#print("new pred: "+ str(np.argmax(NN.forward(newData))))
+out = [x for x in NN.forward(newData)]
+print("new pred: "+ argmax(out))
