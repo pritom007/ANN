65a98b0738c2aac77aa27d16a490375a71228154
Pritom <naitiq@yahoo.com>
2018-12-01 20:22:35 +0800

    experiment network

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
 import database_loader
+import experiment
 import Networks
 import matplotlib.pyplot as plt
-# read data:
 
 
 # mini-batch size:
 mini_batch_size = 10
+# read data:
+training_data, validation_data, test_data = database_loader.load_data()
+
+# Test for the experiment network
+exp_nn = experiment.Network([784, 30, 10], cost=experiment.CrossEntropyCost)
+
+''''
+exp_nn.weight_initializer()
+exp_nn.training(training_data, 4, 10, 0.5, evaluation_data=test_data,
+    monitor_evaluation_accuracy=True,
+    monitor_training_cost=True)
+'''
+
+
+# Test for the given network
 new_nn = Networks.Network([784, 30, 10])
+new_nn.training(training_data, 12, mini_batch_size, .5)
+
+'''
 x_axis=[]
 y_axis=[]
 for x in range(1, 30):
@@ -20,3 +38,4 @@ for x in range(1, 30):
     print("--------------")
 plt.plot(x_axis,y_axis, 'ro')
 plt.show()
+'''
